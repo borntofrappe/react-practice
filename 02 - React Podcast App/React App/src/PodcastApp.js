35bbdf5979b69517668e498dbabee2b2b6a69a53
gabrielecorti <borntofrappe@gmail.com>
2018-12-23 23:56:34 +0100

    complete the UI

--- 02 - React Podcast App/React App/src/PodcastApp.js
+++ 02 - React Podcast App/React App/src/PodcastApp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import CurrentEpisode from './CurrentEpisode.js';
 import styled from 'styled-components';
+import SVGIcons from './SVGIcons.js';
 
 const Podcast = styled.div`
   max-width: 400px;
@@ -34,29 +34,132 @@ const Vinyl = styled.div`
   box-shadow: 0 1px 5px rgba(0, 100, 0, 0.7);
 `;
 
+const CurrentEpisode = styled.div`
+  padding: 1rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const ProgressBar = styled.div`
+  width: 60%;
+  height: 10px;
+  border-radius: 4px;
+  background: linear-gradient(to right, #006400, #006400 0%, #fff 0%);
+  margin-bottom: 2rem;
+`;
+const Controls = styled.div`
+  display: grid;
+  justify-items: center;
+  grid-gap: 1.5rem 2rem;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+const Button = styled.button`
+  width: 48px;
+  height: 48px;
+  background: none;
+  padding: 0.5rem;
+  border-radius: 50%;
+  color: #fff;
+  border: 1.2px solid rgba(255, 255, 255, 0.7);
+  transition: all 0.2s ease-out;
+
+  &: hover {
+  background: rgba(255, 255, 255, 0.15);
+}
+`;
+const ToggleButton = styled(Button)`
+  grid-column: 1/-1;
+  width: 56px;
+  height: 56px;
+`;
+
+const SpeedButton = styled(Button)`
+  font-family: inherit;
+  &:after {
+  content: 'x';
+  }
+`;
+
+const MoreButton = styled(Button)`
+  align-self: flex-end;
+  margin-top: -1rem;
+`;
+
+const Time = styled.p`
+  margin: 1rem 0 2rem;
+  span {
+    margin: 0 0.3rem;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 500;
+
+`;
 // in a wrapping container detail a div just for aesthetics
-// below this empty div, add a component detailing the current episode
-// below this component, a component detailing more episodes
+// below this empty div, add the  components responsible for the application
 class PodcastApp extends Component {
   // in the state detail the audio file
   constructor(props) {
     super(props);
     this.state = {
       audio: 'https://traffic.libsyn.com/freecodecamp/Abbey_and_Erica_interview_.mp3?dest-id=603849',
-      buttons: [
-        'play',
-        'volume',
-        'speed',
-        'stop'
-      ]
+      podcast: [],
+
     };
   }
+
+  parseText(text) {
+    console.log(text);
+  }
+  componentDidMount() {
+    const URL = 'https://podcast.freecodecamp.org/rss';
+
+    fetch(URL)
+      .then(response => response.text())
+      .then(text => this.parseText(text));
+  }
   render() {
-    const { audio, buttons } = this.state;
+    const { audio } = this.state;
     return (
       <Podcast className="PodcastApp">
         <Vinyl />
-        <CurrentEpisode audio={audio} buttons={buttons} />
+        <CurrentEpisode>
+          <ProgressBar />
+
+          <Controls>
+            <ToggleButton>
+              <SVGIcons icon="play" />
+              <audio src={audio} />
+            </ToggleButton>
+
+            <Button>
+              <SVGIcons icon="volume" />
+            </Button>
+
+            <SpeedButton>1</SpeedButton>
+
+            <Button>
+              <SVGIcons icon="stop" />
+            </Button>
+          </Controls>
+
+          <Time>
+            <span>00:00:00</span>
+            /
+            <span>01:09:12</span>
+          </Time>
+
+          <Title>
+            Ep. 51: Erica Peterson, founder, entrepreneur, and mother
+          </Title>
+        </CurrentEpisode>
+
+        <MoreButton>
+          <SVGIcons icon="more" />
+        </MoreButton>
       </Podcast>
     );
   }
