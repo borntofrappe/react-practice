90ba2bac4322bc1ec2a38841ad7826dfa2d04f91
gabrielecorti <borntofrappe@gmail.com>
2019-01-05 00:40:28 +0100

    adjust the title size

--- 02 - FreeCodeCamp Podcast App/React App/src/PodcastTitle.js
+++ 02 - FreeCodeCamp Podcast App/React App/src/PodcastTitle.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 
 // heading dusplaying the title of the current episode
 const Title = styled.h2`
-  font-size: 1.5rem;
+  font-size: 1.35rem;
   font-weight: 500;
+  max-width: 300px;
 `;
 
 // display the title passed through props
