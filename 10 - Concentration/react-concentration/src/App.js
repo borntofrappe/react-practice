1e8d5d8f4fa2e00340a43231aa7b1102b4c6cfc0
borntofrappe <borntofrappe@gmail.com>
2019-10-19 22:59:57 +0200

    set up the project with create-react-app and styled-components

---
+++ 10 - Concentration/react-concentration/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Heading = styled.h1`
+  color: hsl(0, 0%, 95%);
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>Concentration</Heading>
+    </div>
+  );
+}
+
+export default App;
