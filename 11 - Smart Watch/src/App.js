e8e29fe77de1c07461fa6ffe466e73d1d4b2bbe6
borntofrappe <borntofrappe@gmail.com>
2019-10-26 00:44:02 +0200

    include the transition between components

--- 11 - Smart Watch/src/App.js
+++ 11 - Smart Watch/src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import DigitalWatch from './DigitalWatch.js';
 import Counter from './Counter.js';
 
 function App() {
+  const screen = useRef();
+  const [app, setApp] = useState(0);
+
+  function handleClick() {
+    if(!screen.current.classList.contains('Changing')) {
+      screen.current.classList.add('Changing');
+      let timeout = setTimeout(() => {
+        setApp((prev) => prev === 0 ? 1 : 0);
+        screen.current.classList.remove('Changing');
+        clearTimeout(timeout);
+      }, 750)
+    }
+  }
+
   return (
     <div className="Watch">
       <div className="Display">
-        <main className="Screen">
-          {/* <DigitalWatch /> */}
-          <Counter />
+        <main className="Screen" ref={screen}>
+          {
+            app === 0
+            ?
+            <DigitalWatch />
+            :
+            <Counter/>
+          }
         </main>
       </div>
       <div className="Controls">
-        <button aria-label="Previous Gadget"></button>
-        <button aria-label="Next Gadget"></button>
+        <button onClick={handleClick} aria-label="Previous Gadget"></button>
+        <button onClick={handleClick} aria-label="Next Gadget"></button>
       </div>
     </div>
   );
