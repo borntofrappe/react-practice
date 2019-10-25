f2454513905944978a0f23298af3b2e6dd33a1d1
borntofrappe <borntofrappe@gmail.com>
2019-10-26 00:53:46 +0200

    include the analog watch component

---
+++ 11 - Smart Watch/src/AnalogWatch.js
@@ -0,0 +1,63 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+function AnalogWatch() {
+    // initialize two stateful variables to describe the hours and minutes
+    const now = new Date();
+    const [hours, setHours] = useState(now.getHours());
+    const [minutes, setMinutes] = useState(now.getMinutes());
+
+
+    const hoursHands = useRef();
+    const minutesHands = useRef();
+
+    // with the useEffect hook update the rotation of the hands
+    useEffect(() => {
+        hoursHands.current.setAttribute('transform', `rotate(${hours * 30})`);
+        minutesHands.current.setAttribute('transform', `rotate(${minutes * 6})`);
+    }, [hours, minutes]);
+
+    // update the stateful variables at an interval
+    setInterval(() => {
+        const date = new Date();
+        setHours(date.getHours());
+        setMinutes(date.getMinutes());
+    }, 1000);
+
+
+    return(
+        <svg style={{display: 'block', width: '100%', height: 'auto'}} viewBox="0 0 100 100">
+            <defs>
+                <rect id="square" x="-2.5" y="-2.5" width="5" height="5"></rect>
+            </defs>
+            <g transform="translate(50 50)">
+                <g fill="currentColor" stroke="none">
+                    <g id="dial-main">
+                        <use transform="rotate(0) translate(0 -40)" href="#square"></use>
+                        <use transform="rotate(90) translate(0 -40)" href="#square"></use>
+                    </g>
+                    <use href="#dial-main" transform="scale(-1 -1)"></use>
+
+                    <g id="dial-support" opacity="0.5">
+                        <use transform="rotate(30) translate(0 -40) rotate(-30)" href="#square"></use>
+                        <use transform="rotate(60) translate(0 -40) rotate(-60)" href="#square"></use>
+                        <use transform="rotate(120) translate(0 -40) rotate(-120)" href="#square"></use>
+                        <use transform="rotate(150) translate(0 -40) rotate(-150)" href="#square"></use>
+                    </g>
+                    <use href="#dial-support" transform="scale(-1 1)"></use>
+                </g>
+
+                <g fill="none" stroke="currentColor" stroke-linecap="square" stroke-width="4">
+                    <g transform="rotate(0)" ref={hoursHands}>
+                        <path opacity="0.5" d="M 0 0 v -15" stroke-width="4"></path>
+                    </g>
+                    <g transform="rotate(0)" ref={minutesHands}>
+                        <path d="M 0 0 v -30" stroke-width="2.5"></path>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+
+}
+
+export default AnalogWatch;
--- 11 - Smart Watch/src/App.js
+++ 11 - Smart Watch/src/App.js
@@ -1,16 +1,21 @@
 import React, {useState, useRef} from 'react';
 import DigitalWatch from './DigitalWatch.js';
 import Counter from './Counter.js';
+import AnalogWatch from './AnalogWatch.js';
 
 function App() {
   const screen = useRef();
   const [app, setApp] = useState(0);
 
-  function handleClick() {
+  function handleClick(direction) {
     if(!screen.current.classList.contains('Changing')) {
       screen.current.classList.add('Changing');
       let timeout = setTimeout(() => {
-        setApp((prev) => prev === 0 ? 1 : 0);
+        if(direction === 'next') {
+          setApp((prev) => prev === 2 ? 0 : prev + 1);
+        } else {
+          setApp((prev) => prev === 0 ? 2 : prev - 1);
+        }
         screen.current.classList.remove('Changing');
         clearTimeout(timeout);
       }, 750)
@@ -26,13 +31,17 @@ function App() {
             ?
             <DigitalWatch />
             :
+            app === 1
+            ?
             <Counter/>
+            :
+            <AnalogWatch/>
           }
         </main>
       </div>
       <div className="Controls">
-        <button onClick={handleClick} aria-label="Previous Gadget"></button>
-        <button onClick={handleClick} aria-label="Next Gadget"></button>
+        <button onClick={() => handleClick('prev')} aria-label="Previous Gadget"></button>
+        <button onClick={() => handleClick('next')} aria-label="Next Gadget"></button>
       </div>
     </div>
   );
