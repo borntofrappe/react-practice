3db0b02272bd7d86891e3d4f70e66d84ebded50a
borntofrappe <borntofrappe@gmail.com>
2020-01-09 23:23:24 +0100

    set up landing page

--- 19 - Exact Change/src/App.js
+++ 19 - Exact Change/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import Header from './Header.js'
+import React, { useState } from 'react'
+import LandingPage from './LandingPage.js'
+import Game from './Game.js'
+import styled from 'styled-components'
+
+const Root = styled.div`
+  background: hsl(0, 0%, 100%);
+  color: hsl(0, 0%, 20%);
+  padding: 3.5rem;
+  border-radius: 20px;
+  box-shadow: 0 0 10px -6px hsl(0, 0%, 50%);
+`
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const play = () => setIsPlaying(true);
+
   return (
-    <>
-      <Header />
-    </>
+    <Root>
+      {
+        isPlaying
+        ?
+        <Game />
+        :
+        <LandingPage play={play} />
+      }
+    </Root>
   );
 }
 
--- 19 - Exact Change/src/LandingPage.js
+++ 19 - Exact Change/src/LandingPage.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Header = styled.header`
+const Section = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
   line-height: 2;
-  font-family: "Comfortaa", cursive;
+  font-family: 'M PLUS Rounded 1c', sans-serif;
+  & > * + * {
+    margin-top: 1rem;
+  }
 `
 const Heading = styled.h1`
   font-size: 2.5rem;
   text-transform: capitalize;
+
+  &::selection {
+    background: hsl(30, 95%, 60%);
+  }
 `
 
 const Illustration = styled.svg`
@@ -20,9 +27,36 @@ const Illustration = styled.svg`
   display: block;
 `
 
-export default () => <Header>
+const Button = styled.button`
+  color: inherit;
+  padding: 0.25rem;
+  border-radius: 50%;
+  background: none;
+  border: 0.4rem solid currentColor;
+  width: 3.5rem;
+  height: 3.5rem;
+  display: block;
+  transition: transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.5), color 0.3s ease-in-out;
+
+  svg {
+    width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  &:focus {
+    outline: none;
+  }
+  &:focus,
+  &:hover {
+    color: hsl(30, 95%, 50%);
+    transform: scale(1.2);
+  }
+`
+
+export default ({play}) => <Section>
   <Illustration viewBox="0 0 135 100" width="135" height="100">
-  <g fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
+  <g fill="none" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round">
     <g transform="translate(7 86)">
       <g id="coin">
         <rect width="65" height="12" rx="1" />
@@ -34,7 +68,7 @@ export default () => <Header>
       <use href="#coin" x="-5" y="-28" />
     </g>
 
-    <g stroke-width="5">
+    <g strokeWidth="5">
       <g transform="translate(70 30) rotate(10)">
         <circle r="27.5" />
         <path d="M 0 -14 a 14 14 0 1 0 14 14" />
@@ -49,4 +83,9 @@ export default () => <Header>
   </Illustration>
   <Heading>Exact change</Heading>
 
-</Header>
+  <Button onClick={() => play()}>
+    <svg viewBox="-50 -50 100 100" width="30" height="30">
+      <path fill="currentColor" stroke="currentColor" strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" d="M -15 -30 l 40 30 -40 30 z"/>
+    </svg>
+  </Button>
+</Section>
