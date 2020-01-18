c8a3f3db7ec4b7e6f1052a50ba9a901e72ad4ed3
borntofrappe <borntofrappe@gmail.com>
2020-01-18 23:34:00 +0100

    set up counter

--- 20 - Binary Counter/src/App.js
+++ 20 - Binary Counter/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const timeoutRef = useRef()
+  function tick() {
+    setCount(count + 1)
+  }
+  timeoutRef.current = setTimeout(() => {
+    tick();
+  }, 1000);
+
+  useEffect(() => {
+    clearTimeout(timeoutRef)
+  }, [timeoutRef])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Row">
+      {count.toString(2)}
     </div>
+
   );
 }
 
