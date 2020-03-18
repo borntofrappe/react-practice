52cf201a50dbbb4f11d6fab4b7ab465086090a21
borntofrappe <borntofrappe@gmail.com>
2020-03-19 00:13:57 +0100

    complete visualization

--- 21 - Line Charts/src/App.js
+++ 21 - Line Charts/src/App.js
@@ -1,5 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import LineChart from './LineChart';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > * {
+    margin-top: 1rem;
+  }
+`;
+
+const Form = styled.form`
+  background: hsl(0, 0%, 100%);
+  box-shadow: 0 2px 10px -7px hsla(0, 0%, 0%, 0.8);
+  padding: 28px 36px;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+
+  &:not(:first-of-type) {
+    margin-top: 1rem;
+  }
+`;
+
+const Select = styled.select`
+  font-family: inherit;
+  font-size: 1.2rem;
+  color: inherit;
+  padding: 0.2rem 0.5rem;
+`;
 
 function App() {
   const url = "https://api.covid19api.com";
@@ -34,36 +69,36 @@ function App() {
   }, [country, status]);
 
   return (
-    <>
+    <Container>
       {
         countries.length > 0 ?
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label>
-          Select country
-          <select onInput={(e) => setCountry(e.target.value)}>
+      <Form onSubmit={(e) => e.preventDefault()}>
+        <Label>
+          Country
+          <Select onInput={(e) => setCountry(e.target.value)}>
             <option>---</option>
             {
             countries.map(({country, slug}) => <option value={slug} key={slug}>{country}</option>)
             }
-          </select>
-        </label>
-        <label>
-          Select status
-          <select onInput={(e) => setStatus(e.target.value)}>
+          </Select>
+        </Label>
+        <Label>
+          Status
+          <Select onInput={(e) => setStatus(e.target.value)}>
             {
               statuses.map(status => <option value={status} key={status}>{status}</option>)
             }
-          </select>
-        </label>
-    </form>
+          </Select>
+        </Label>
+    </Form>
         : null
       }
     {
       data.length > 0 &&
-      <LineChart data={data} />
+      <LineChart country={country} status={status} data={data} />
     }
 
-    </>
+    </Container>
   );
 }
 
--- 21 - Line Charts/src/LineChart.js
+++ 21 - Line Charts/src/LineChart.js
@@ -1,33 +1,95 @@
 import React from 'react';
-import { scaleLinear, scaleTime, extent, max, line } from 'd3';
+import { scaleLinear, scaleTime, max, min, line, format, timeFormat, ticks } from 'd3';
+import styled from 'styled-components';
 
-function LineChart({data}) {
-  const margin = 25;
-  const width = 400;
+const Main = styled.main`
+  background: hsl(0, 0%, 100%);
+  box-shadow: 0 2px 10px -7px hsla(0, 0%, 0%, 0.8);
+  padding: 20px 28px;
+  line-height: 1.25;
+`;
+
+const H1 = styled.h1`
+  font-size: 2rem;
+  text-transform: uppercase;
+  font-weight: initial;
+`;
+
+const H2 = styled.h2`
+  font-size: 3.25rem;
+  font-weight: bold;
+  span {
+    margin-left: -5px0;
+    font-size: 1rem;
+    font-weight: initial;
+  }
+`;
+
+const SVG = styled.svg`
+  display: block;
+  width: 90vw;
+  max-width: 400px;
+  height: auto;
+
+  text {
+    text-transform: capitalize;
+  }
+`
+
+function LineChart({country, status, data}) {
+  const margin = 30;
+  const width = 600;
   const height = 400;
 
 
+  const total = data.reduce((acc, curr) => acc + curr.cases, 0);
+  const formatNumber = format(",");
+  const formatDay = timeFormat("%d/%m");
+
+  const maxCases = max(data, d => d.cases);
   const yScale = scaleLinear()
-    .domain([0, max(data, d => d.cases)])
+    .domain([0, maxCases])
     .range([height, 0]);
 
+    const dayOne = min(data, d => new Date(d.date));
+    const dayLast = max(data, d => new Date(d.date));
   const xScale = scaleTime()
-    .domain(extent(data, d => new Date(d.date)))
+    .domain([dayOne, dayLast])
     .range([0, width]);
 
   const lineFunction = line()
     .x(d => xScale(new Date(d.date)))
     .y(d => yScale(d.cases));
 
+  const ticksY = ticks(0, maxCases, 4);
+  const ticksX = data.filter((d, i) => i % 5 === 0).map(({date}) => date);
+
   return (
-    <svg viewBox={`0 0 ${width + margin * 2} ${height + margin * 2}`} width={width} height={height}>
-      <g transform={`translate(${margin} ${margin})`}>
-        <text>{data.reduce((acc, curr) => acc + curr.cases, 0)} Cases</text>
-        <g fill="none" stroke="currentColor" strokeWidth="5">
-          <path d={lineFunction(data)} />
+    <Main>
+      <H1>{country}</H1>
+      <H2>{formatNumber(total)} <span>{status}</span></H2>
+      <SVG viewBox={`0 0 ${width + margin * 2} ${height + margin * 2}`} width={width} height={height}>
+        <g transform={`translate(${margin} ${margin})`}>
+          <g>
+            {
+              ticksY.map(tick => <g key={`y-${tick}`}>
+                  <path fill="none" stroke="currentColor" strokeDasharray="2 3" opacity="0.3" d={`M 0 ${yScale(tick)} h ${width}`} />
+                  <text fontSize="25" y={yScale(tick) - 5}>{formatNumber(tick)}</text>
+                </g>)
+            }
+            {
+              ticksX.map(tick => <g key={`x-${tick}`} transform={`translate(${xScale(new Date(tick))} ${height})`}>
+                  <path fill="none" stroke="currentColor" opacity="0.3" d={`M 0 0 v 5`} />
+                  <text fontSize="25" y="25">{formatDay(new Date(tick))}</text>
+                </g>)
+            }
+          </g>
+          <g fill="none" stroke="currentColor" strokeWidth="5">
+            <path d={lineFunction(data)} />
+          </g>
         </g>
-      </g>
-    </svg>
+      </SVG>
+    </Main>
   )
 }
 
