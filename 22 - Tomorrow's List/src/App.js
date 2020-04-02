de1aa889245d502110b44015cfb1b3994046fcbf
borntofrappe <borntofrappe@gmail.com>
2020-04-02 10:36:15 +0200

    style application with material ui

--- 22 - Tomorrow's List/src/App.js
+++ 22 - Tomorrow's List/src/App.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import List from './List';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import AddBox from '@material-ui/icons/AddBox';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import DeleteForever from '@material-ui/icons/DeleteForever'
 
 function App() {
   const [value, setValue] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([{
+    value: 'grocery',
+    id: 2,
+  }]);
 
   function updateValue(e) {
     setValue(e.target.value)
@@ -12,10 +21,10 @@ function App() {
   function updateList(e) {
     e.preventDefault();
     if(value) {
-      setList([...list, {
+      setList([{
         value,
         id: Math.random(),
-      }]);
+      }, ...list]);
       setValue('');
     }
   }
@@ -32,31 +41,37 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Tomorrow</h1>
-      <form onSubmit={updateList}>
-        <label>
-          New task
-          <input
-            value={value}
-            onChange={updateValue}
-            name="task"
-            id="task"
-            type="text"
-            placeholder="Clean room" />
-        </label>
+    <Container style={{margin: "2rem auto", maxWidth: "30em"}}>
+      <Box component="form" onSubmit={updateList} style={{ display: 'flex', marginBottom: "1.5rem" }}>
+        <TextField
+          style={{flexGrow: '1'}}
+          value={value}
+          onChange={updateValue}
+          required
+          name="task"
+          id="task"
+          type="text"
+          label="New task"
+          variant="outlined" />
 
-        <button>Add</button>
-      </form>
+        <Button variant="contained" color="primary" disableElevation style={{ marginLeft: "0.4rem "}}>
+          Add
+          <AddBox
+            style={{ fontSize: "1.75em", marginLeft: "0.5rem", lineHeight: "1.75em"}} />
+        </Button>
+      </Box>
 
       {list.length > 0 && <>
         <List
           list={list}
           removeValueById={removeValueById} />
 
-        <button onClick={clearList}>Clear</button>
+        <Button style={{marginTop: "1.5rem"}} variant="contained" disableElevation color="secondary" onClick={clearList}>
+          Clear List
+          <DeleteForever />
+        </Button>
       </>}
-    </div>
+    </Container>
   );
 }
 
--- 22 - Tomorrow's List/src/List.js
+++ 22 - Tomorrow's List/src/List.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import DeleteOutlined from '@material-ui/icons/DeleteOutlined'
 
-export default ({list, removeValueById}) => <ul>
-  {list && list.map(({value, id}) => <li key={id}>
-    <p>{value}</p>
-    <button onClick={() => removeValueById(id)}>Remove</button>
-  </li>)}
-</ul>
+export default ({list, removeValueById}) => <Grid container direction="row" component="ul">
+  {list && list.map(({value, id}) => <Grid style={{ marginBottom: "0.75rem"}} xs={4} component="li" key={id}>
+    <p style={{fontSize: "1.5rem", marginBottom: "0.5rem"}}>{value}</p>
+    <Button
+      variant="outlined"
+      disableElevation
+      onClick={() => removeValueById(id)}>
+        Remove
+        <DeleteOutlined />
+      </Button>
+  </Grid>)}
+</Grid>
